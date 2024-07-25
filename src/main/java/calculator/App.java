35e3931ca0efb322a1dc094408837480cdc8305a
Ackrilll <skawlsgus2@naver.com>
2024-07-25 13:34:18 +0900

    lv 1-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        // LV 1-1
         Scanner sc = new Scanner(System.in);
 
         System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -13,6 +14,14 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num2 = sc.nextInt();
+
+        //LV 1-2
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+        String operation = sc.next();
+
+        //LV 1-3
+
     }
 }
 
